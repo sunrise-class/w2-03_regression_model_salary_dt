610e425742c4d6c801601fecfddf2b4cd0b38f6c
Pech Channary <pechnary15@gmail.com>
2024-07-31 18:24:22 +0700

    react jenkins

---
+++ Desktop/react-restaurant-landing-page/README.md
@@ -0,0 +1,6 @@
+A simple landing page - first designed in figma and then coded in React. Hope you like it.
+
+this project is running on port 3000 ok na
+
+this is my first try ><
+hello this is my first try on jenkins ><
---
+++ Desktop/react-restaurant-landing-page/src/Components/Testimonial.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ProfilePic from "../Assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonial = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+          I get way too much happiness from good food.We're all so busy we don't
+          make time to enjoy our lives, good company and good food.
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+        <h2>Ginna Moon</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
